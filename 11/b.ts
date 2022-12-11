118ec1a9c1a807f4cc8986c719d43ccd0a7e9db6
Dirivial <alexander.kadeby@gmail.com>
2022-12-11 07:29:33 +0100

    A bit more concise (also, monkeh)

--- 11/b.ts
+++ 11/b.ts
@@ -2,11 +2,10 @@ const _input = await Deno.readTextFile("./input.txt");
 const inputArray = _input.split("\n\n");
 
 type Monkey = {
-  name: number;
   items: number[];
   operation: string;
   operationBy: string;
-  test: number;
+  divisibleBy: number;
   ifTrue: number;
   ifFalse: number;
   timesInspect: number;
@@ -15,83 +14,49 @@ type Monkey = {
 const monkeys: Monkey[] = [];
 
 inputArray.forEach((monkey, index) => {
+  const lines = monkey.split("\n").map((v) => v.trim());
+
   monkeys.push({} as Monkey);
   monkeys[index].timesInspect = 0;
-  monkey.split("\n").forEach((line, i) => {
-    if (line.match("Monkey")) {
-      monkeys[index].name = Number(line.split(" ")[1]);
-    } else if (line.match("items")) {
-      monkeys[index].items = line
-        .trim()
-        .split(" ")
-        .splice(2)
-        .map((v) => v.replace(",", ""))
-        .map((v) => Number(v));
-    } else if (line.match("Operation")) {
-      const operation = line.split("= ")[1];
-      monkeys[index].operation = operation[4];
-      monkeys[index].operationBy = operation.slice(6);
-    } else if (line.match("Test")) {
-      const test = line.trim().split(" ");
-      monkeys[index].test = Number(test[3]);
-    } else if (line.match("true")) {
-      monkeys[index].ifTrue = Number(line.trim().split(" ")[5]);
-    } else if (line.match("false")) {
-      monkeys[index].ifFalse = Number(line.trim().split(" ")[5]);
-    }
-  });
+  monkeys[index].items = lines[1]
+    .split(" ")
+    .splice(2)
+    .map((v) => v.replace(",", ""))
+    .map((v) => Number(v));
+
+  const operation = lines[2].split("= ")[1];
+  monkeys[index].operation = operation[4];
+  monkeys[index].operationBy = operation.slice(6);
+
+  const divisibleBy = lines[3].split(" ");
+  monkeys[index].divisibleBy = Number(divisibleBy[3]);
+  monkeys[index].ifTrue = Number(lines[4].split(" ")[5]);
+  monkeys[index].ifFalse = Number(lines[5].split(" ")[5]);
 });
 
-const allDivisibleBy = monkeys.reduce((a, b) => a * b.test, 1);
+const allDivisibleBy = monkeys.reduce((a, b) => a * b.divisibleBy, 1);
 
 for (let i = 0; i < 10000; i++) {
   monkeys.forEach((monkey) => {
     while (monkey.items.length > 0) {
-      let result = 0;
+      let result = monkey.items[0];
       switch (monkey.operation) {
         case "+":
-          result =
-            monkey.items[0] +
-            Number(
-              monkey.operationBy === "old"
-                ? monkey.items[0]
-                : monkey.operationBy
-            );
-          break;
-        case "-":
-          result =
-            monkey.items[0] -
-            Number(
-              monkey.operationBy === "old"
-                ? monkey.items[0]
-                : monkey.operationBy
-            );
+          result += Number(
+            monkey.operationBy === "old" ? monkey.items[0] : monkey.operationBy
+          );
           break;
         case "*":
-          result =
-            monkey.items[0] *
-            Number(
-              monkey.operationBy === "old"
-                ? monkey.items[0]
-                : monkey.operationBy
-            );
-          break;
-        case "/":
-          result =
-            monkey.items[0] /
-            Number(
-              monkey.operationBy === "old"
-                ? monkey.items[0]
-                : monkey.operationBy
-            );
+          result *= Number(
+            monkey.operationBy === "old" ? monkey.items[0] : monkey.operationBy
+          );
           break;
       }
       result = result % allDivisibleBy;
-      if (result % monkey.test === 0) {
-        monkeys[monkey.ifTrue].items.push(result);
-      } else {
-        monkeys[monkey.ifFalse].items.push(result);
-      }
+      result % monkey.divisibleBy === 0
+        ? monkeys[monkey.ifTrue].items.push(result)
+        : monkeys[monkey.ifFalse].items.push(result);
+
       monkey.items.shift();
       monkey.timesInspect++;
     }
